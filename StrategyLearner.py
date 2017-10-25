4a36fe63ca519f1b318d2bd1c26de9e1a6340def
Wenchen Li <wenchen.li.cs@gmail.com>
2017-10-25 15:48:15 +0800

    clean up a bit

--- StrategyLearner.py
+++ StrategyLearner.py
@@ -92,13 +92,16 @@ class StrategyLearner(object):
     def get_current_portfolio_values(self, today_stock_price):
         return self.portfolio[CASH] + self.portfolio[STOCK] * today_stock_price
 
+    def init_portfolio(self, sv,):
+        self.portfolio[CASH] = float(sv)
+        self.portfolio[STOCK] = 0
+
     def addEvidence(self, symbol="SPY",
                     sd=dt.datetime(2008, 1, 1),
                     ed=dt.datetime(2009, 1, 1),
                     sv=10000):
 
-        self.portfolio[CASH] = float(sv)
-        self.portfolio[STOCK] = 0
+        self.init_portfolio(sv)
 
         # record in panda format
         dates = pd.date_range(sd, ed)
@@ -110,19 +113,19 @@ class StrategyLearner(object):
 
         tf = RawTradeFeatures(symbol, sd, ed)
         price, price_SPY = tf.get_adj_price()
-        # vol,vol_SPY = tf.get_vol()
 
+        # buy and hold the benchmark
         benchmark_price = price_SPY.as_matrix()
         benchmark_values = prices_all[[symbol, ]]  # only portfolio symbols
         benchmark_num_stock = int(sv / benchmark_price[0])
         cash = sv - float(benchmark_num_stock * benchmark_price[0])
         for i, p in enumerate(benchmark_price):
             benchmark_values.values[i, :] = cash + p * benchmark_num_stock
+
         # Discretize the values of the features, this part can replaced by deep learning
 
         # macd
         macd = ut.norm(ut.get_macd(prices_all[symbol])["MACD"].as_matrix())
-        # macd = macd.reshape((-1, 1))
 
         # bollinger band
         bb = ut.Bollinger_Bands_given_sym_dates([symbol], sd, ed)
@@ -133,13 +136,8 @@ class StrategyLearner(object):
         # normalize
         l = price[symbol].as_matrix()
         price_array = l.copy()
-        X = ut.norm(l)  # normalize, get between 0-1
 
-        # combine all
-        # x = X.reshape((-1, 1))  # make it 2d
-        x = zip(X, macd, bb_ub, bb_lb)
-        x = ut.norm(x)
-        # x = zip(bb_ub,bb_lb)
+        #final factor sent to model
         x = macd.reshape((-1, 1))
 
         # discretize
@@ -156,13 +154,10 @@ class StrategyLearner(object):
 
         # value iteration
         for k in xrange(self.num_epoch):
-            self.records.append([])  # recording new epoch transactions
-
             for i, s in enumerate(feature_states):
                 if i == len(feature_states) - 1:
                     portfolio_value.values[i, :] = self.get_current_portfolio_values(price_array[-1])
                     continue  # skip last because 2nd day price
-
                 # compute the current state(include holding)
                 current_holding_state = self.current_holding_state
                 current_feature_state = s
@@ -172,7 +167,6 @@ class StrategyLearner(object):
                 r = self.last_r
                 if r < 0:  # punish negative reward
                     r *= self.negative_return_punish_factor
-
                 # Query the learner with the current state and reward to get an action
                 action = self.learner.query(current_state, r)
 
@@ -217,7 +211,6 @@ class StrategyLearner(object):
                 portfolio_value.values[i, :] = self.get_current_portfolio_values(price_array[i])
                 self.last_r = (self.get_current_portfolio_values(price_array[i + 1]) - last_portfolio_value) / float(sv)
                 if self.verbose: print self.last_r
-                self.records[k].append((current_state, action, self.last_r))
 
             if self.verbose: print "epoch", k, " current cumulated return:", self.get_current_portfolio_values(
                 price_array[-1]) / float(sv) - 1.0, "portfolio:", self.portfolio
@@ -231,7 +224,6 @@ class StrategyLearner(object):
                 self.portfolio[STOCK] = 0
                 self.current_holding_state = 0
                 self.last_r = 0.0
-
                 # decay alpha
                 self.learner.decay_alpha()
 
@@ -243,7 +235,6 @@ class StrategyLearner(object):
                      save_dir=self.current_working_dir)
         ut.plot_data(p_value_all, title="portfolio value_train", ylabel="USD", save_image=True,
                      save_dir=self.current_working_dir)
-        # ut.save(self.records,self.current_working_dir+training_record_save_name)
         self.learner.save_model(table_name=self.current_working_dir + dyna_q_trader_model_save_name)
 
         trade_return = self.get_current_portfolio_values(price_array[-1]) / sv - 1.0
@@ -257,9 +248,7 @@ class StrategyLearner(object):
                    ed=dt.datetime(2010, 1, 1),
                    sv=10000):
 
-        k = 0  # for the self.records[0]
-        self.portfolio[CASH] = float(sv)
-        self.portfolio[STOCK] = 0
+        self.init_portfolio(sv)
 
         # record in panda format
         dates = pd.date_range(sd, ed)
@@ -270,7 +259,6 @@ class StrategyLearner(object):
         # Select several technical features, and compute their values for the training data
         tf = RawTradeFeatures(symbol, sd, ed)
         price, price_SPY = tf.get_adj_price()
-        # vol, vol_SPY = tf.get_vol()
 
         # buy and hold benchmark
         benchmark_price = price_SPY.as_matrix()
@@ -293,14 +281,9 @@ class StrategyLearner(object):
         # normalize
         l = price[symbol].as_matrix()
         price_array = l.copy()
-        X = ut.norm(l)  # normalize, get between 0-1
 
         # combine all
-        # x = X.reshape((-1, 1))  # make it 2d
-        x = zip(X, macd, bb_ub, bb_lb)
-        x = ut.norm(x)
         x = macd.reshape((-1, 1))
-        # x = zip(bb_ub, bb_lb)
 
         # kmeans model load
         kmeans_model = pickle.load(open(self.current_working_dir + kmeans_model_save_name, 'rb'))
@@ -313,9 +296,8 @@ class StrategyLearner(object):
         self.cumulated_return_indicator = 0
         # load trained q table(if dyna,t_table and r_table)
         self.learner.load_model(table_name=self.current_working_dir + dyna_q_trader_model_save_name)
-        # value iteration
 
-        self.records.append([])
+        # value iteration
         for i, s in enumerate(feature_states):
             if i == len(feature_states) - 1:
                 portfolio_value.values[i, :] = self.get_current_portfolio_values(price_array[-1])
@@ -372,10 +354,6 @@ class StrategyLearner(object):
 
             self.last_r = (self.get_current_portfolio_values(price_array[i + 1]) - last_portfolio_value) / float(sv)
             if self.verbose: print self.last_r
-            # if self.last_r < 0:self.last_r *=2 # tell q trader, negative loss is bad
-
-            self.records[k].append((current_state, action, self.last_r))
-            # print "reward:", self.last_r
 
         self.last_cumulated_return = self.get_current_portfolio_values(price_array[-1]) / float(sv) - 1
 
@@ -388,7 +366,6 @@ class StrategyLearner(object):
                      save_dir=self.current_working_dir)
         ut.plot_data(p_value_all, title="portfolio value_test", ylabel="USD", save_image=True,
                      save_dir=self.current_working_dir)
-        # print self.last_cumulated_return
         trade_return = self.get_current_portfolio_values(price_array[-1]) / sv - 1.0
         if self.verbose: print "cumulated return=:", trade_return
         return trades, trade_return
